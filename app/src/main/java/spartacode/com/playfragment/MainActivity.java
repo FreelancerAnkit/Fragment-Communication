7c7adf4223edf0e33cdfea50f4fdac47f03e67a7
Ankit <palliankit123@gmail.com>
2018-12-07 20:39:49 +0530

    Again Removed Comment 2

--- app/src/main/java/spartacode/com/playfragment/MainActivity.java
+++ app/src/main/java/spartacode/com/playfragment/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity extends AppCompatActivity implements FragmentA.Communi
         fm = getSupportFragmentManager();
         fm.beginTransaction().add(R.id.layout_container,fragmentB,"fragment2").commit();
         fm.beginTransaction().add(R.id.layout_container,fragmentA,"fragment1").commit();
-        Log.e("Hey", "onCreate: " );
     }
 
     @Override
